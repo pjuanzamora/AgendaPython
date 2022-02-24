a42de7c02ed41aadbcedae425d89daa98c693b2a
Juanfran <pjuanzamora@gmail.com>
2022-02-24 10:19:17 +0100

    Función añadir y ver todos terminados

--- agenda.py
+++ agenda.py
@@ -1,5 +1,6 @@
 #Agenda de teléfonos en python
 
+#Muestra el menú con las opciones y devuelve la opción
 def mostrarMenu():
     opc = 0
     while (opc < 1 or opc > 6):
@@ -18,4 +19,40 @@ def mostrarMenu():
             print("********* Opción no válida ************")
     return opc
 
+#Mostrar todos los contactos de la agenda
+def mostrarTodos(vAgenda):
+    for contacto in vAgenda:
+        print(contacto)
 
+#Añadir un contacto a la agenda con el formato nombre-telefono en una lista
+def addcontact(vAgenda):
+    print("Guardando contacto")
+    nombre = input("Dime el nombre del contacto: ")
+    tel = input("Dime el telefono del contacto: ")
+    contacto = nombre+"-"+tel
+    vAgenda.append(contacto)
+
+
+
+print("*******  AGENDA DE TELÉFONOS TIC2 *********")
+vAgenda = []
+opc = mostrarMenu()
+while (opc != 6):
+    if (opc==1):
+        addcontact(vAgenda)
+        print("Opcion 1")
+    elif (opc==2):
+        print("Opcion 2")
+    elif (opc==3):
+        print("Opcion 3")
+    elif (opc==4):
+        print("Opcion 4")
+    else:
+        print("Contactos guardados:")
+        mostrarTodos(vAgenda)
+
+    opc = mostrarMenu()
+   
+print("Saliendo de la agenda... Encriptando datos por seguridad")
+    
+    
